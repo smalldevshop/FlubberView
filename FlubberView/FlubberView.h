6527d065635165ed57a2c51ba79dbd575488ed53
mattthousand <matt.buckley@raizlabs.com>
2016-09-18 18:19:59 -0400

    Create FlubberView framework.

---
+++ FlubberView/FlubberView.h
@@ -0,0 +1,19 @@
+//
+//  FlubberView.h
+//  FlubberView
+//
+//  Created by Matthew Buckley on 9/18/16.
+//  Copyright © 2016 Nice Things. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlubberView.
+FOUNDATION_EXPORT double FlubberViewVersionNumber;
+
+//! Project version string for FlubberView.
+FOUNDATION_EXPORT const unsigned char FlubberViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlubberView/PublicHeader.h>
+
+
